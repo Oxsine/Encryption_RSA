97366dc24ee41be97286794b0f28281fcf0ece9e
Oxsine <Oxsine90@mail.ru>
2023-10-13 17:25:44 +0300

    add word input

---
+++ RSA.py
@@ -0,0 +1,37 @@
+alp = {
+'A': 1,
+'B': 2,
+'C': 3,
+'D': 4,
+'E': 5,
+'F': 6,
+'G': 7,
+'H': 8,
+'I': 9,
+'J': 10,
+'K': 11,
+'L': 12,
+'M': 13,
+'N': 14,
+'O': 15,
+'P': 16,
+'Q': 17,
+'R': 18,
+'S': 19,
+'T': 20,
+'U': 21,
+'V': 22,
+'W': 23,
+'X': 24,
+'Y': 25,
+'Z': 26
+}
+vod = str.upper((input()))
+tapok = tuple(vod)
+count = 0
+while count < len(tapok):
+    if tapok[count] in alp.keys():
+        print(alp[tapok[count]])
+    else:
+        print(False)
+    count+=1
